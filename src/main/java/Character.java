9c101d194058cafb994315584c862e6cff4d0f34
andresian07 <andresianzulu@gmail.com>
2026-03-24 13:02:06 +0100

    retobit listo

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,14 +1,48 @@
-class Character implements Combatant {
+public abstract class Character implements Combatant {
     // 🗒️ PROPIEDADES
     protected String name;
     protected int health;
+    protected Status status;
     // le falta el status
 
     // 🏗️ CONSTRUCTOR
     protected Character(String name, int health) {
         this.name = name;
         this.health = health;
+        this.status = Status.REGULAR;
         // el status inicial debe ser siempre REGULAR
     }
 
+    @Override
+    public void receiveDamage(int damage){
+        this.health -= damage;
+
+        if (this.health <= 0){
+            this.health = 0;
+            this.status = Status.DEAD;
+        }
+
+
+    }
+
+    @Override
+    public boolean isAlive(){
+        return this.health > 0;
+        }
+
+    @Override
+    public int getCurrentHealth() {
+        return this.health;
+    }
+
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getStatus() {
+        return this.status.name();
+    }
 }
